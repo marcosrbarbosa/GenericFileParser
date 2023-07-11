dc6976764ed9f42b4f3321d54788a56cfc219c0f
Marcos Roberto Barbosa <marcos_barbosa@hotmail.com>
2023-07-11 11:30:00 -0300

    Added Extensions

---
+++ Extensions/DoubleExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Extensions
+{
+	internal static class DoubleExtension
+	{
+		public static double ToExcelRoundedDouble(this double value) 
+		{
+			return Math.Round(value, 2, MidpointRounding.AwayFromZero);
+		}
+	}
+}
---
+++ Extensions/EnumDescriptionException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Extensions
+{
+	internal static class EnumDescriptionException
+	{
+		public static string GetEnumDescription<T>(this T enumValue) where T : struct 
+		{
+			Type tp= enumValue.GetType();
+			if (tp.IsEnum) {
+				var memberInfo = tp.GetMember(enumValue.ToString());
+				if (memberInfo != null && memberInfo.Length > 0) 
+				{
+					var memberAttributes = memberInfo[0].GetCustomAttributes(typeof(DescriptionAttribute),false);
+					if (memberAttributes != null && memberAttributes.Length > 0)
+					{
+						return ((DescriptionAttribute) memberAttributes[0]).Description;
+					}
+				}
+			}
+			throw new ArgumentException("enumValue must be of Enum type");
+		}
+	}
+}
